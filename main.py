805356977616cf658fef337416066e748c4898b3
Winwin <em.ireri41@gmail.com>
2024-01-31 12:35:01 +0300

    make the code much better

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ def get_rgb_arrays(img):
     for y in range(height):
         for x in range(width):
             r_array[y][x], g_array[y][x], b_array[y][x] = img.getpixel((x, y))
+    return r_array, g_array, b_array, width, height
+def process_pixel(r, g, b):
+    global instruction_pointer, pointer_direction, stack
 
-    return r_array, g_array, b_array
-def process_pixel(r, g, b, ip=instruction_pointer, pd=pointer_direction, stack=stack):
-    match (b):
+    #print("[DBG]", r, g, b, instruction_pointer, pointer_direction, stack)
+
+    match b:
         case 0:
-            [ip + pd, pd] # "dead code" -someone (they are utterly wrong, as this is a noop instruction.)
+            a = stack.pop()
+            stack.append(a)
+            # do litterally nothing
         case 2:
             stack.append((r or stack.pop()) + (g or stack.pop()))
         case 3:
@@ -34,33 +39,34 @@ def process_pixel(r, g, b, ip=instruction_pointer, pd=pointer_direction, stack=s
 
         case 248:
             a = stack.pop()
-            stack.append(a)
-            stack.append(a)
+            stack.extend([a, a])
         case 249:
             if (r or stack.pop()) == 0:
-                ip += (pd*2)
-        
+                instruction_pointer = [instruction_pointer[0] + pointer_direction[0], instruction_pointer[1] + pointer_direction[1]]
         case 250:
             stack.pop()
         case 251:
-            pd = [0, -1]
+            pointer_direction = [0, -1]
         case 252:
-            pd = [0, 1]
+            pointer_direction = [0, 1]
         case 253:
-            pd = [-1, 0]
+            pointer_direction = [-1, 0]
         case 254:
-            pd = [1, 0]
+            pointer_direction = [1, 0]
         case 255:
             sys.exit(0)
         case _:
-            stack.append(r)
-            stack.append(g)
-    return [ip + pd, pd]
+            stack.extend([r, g])
+    
+    instruction_pointer = [instruction_pointer[0] + pointer_direction[0], instruction_pointer[1] + pointer_direction[1]]
 if len(sys.argv) != 2:
     print("I'm quitting execution because you didn't give me an image. There used to be documentation here, but i cant keep up with writing two of them.")
     sys.exit(1)
 image_path = sys.argv[1]
-r_values, g_values, b_values = get_rgb_arrays((Image.open(image_path)))
-for y in range(len(r_values)):
-    for x in range(len(r_values[y])):
-        instruction_pointer, pointer_direction = process_pixel(r_values[y][x], g_values[y][x], b_values[y][x])
+r_values, g_values, b_values, width, height = get_rgb_arrays((Image.open(image_path)))
+ #todo: make this actually correct
+while True:
+    x, y = instruction_pointer
+    x = x%width
+    y = y%height 
+    process_pixel(r_values[y][x], g_values[y][x], b_values[y][x])
